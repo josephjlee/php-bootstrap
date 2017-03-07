6b61da40285329c0c69ac71a5c2677cb959676cf
hafismuh <hafismuh@gmail.com>
2017-03-07 20:35:58 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel=”icon” type=”image/png” href="assets/img/favicon.png">
+    <title>PHP & Bootstrap theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/navbar-static-top.css" rel="stylesheet">
+    <link href="assets/css/sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+
+  <body>
+  <?php
+  include 'menu.php';
+  ?>
+    <?php
+    ?>
+  <div class="container well">
+      <div class="blog-header">
+        <h1 class="blog-title">Aplikasi CRUD dengan Bootstrap</h1>
+        <p class="lead blog-description">Bootstrap merupakan tema </p>
+      </div>
+      <div class="row">
+      <div class="col-sm-12 blog-main">
+      </div>
+      </div>
+    </div>
+    <footer class="footer" >
+      <div class="container" align="center">
+      <p><b>Aplikasi CRUD dengan Bootstrap</b></p>
+      <p><b>&copy; 2016 <a href="https://twitter.com/hafismuh" target="_blank">hfsmh.com</a></p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+  </body>
+</html>
