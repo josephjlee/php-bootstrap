5b6347632a21bbc8bf1890f1ee5784e731f2f363
hafismuh <hafismuh@gmail.com>
2017-03-07 20:36:16 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,37 @@
+<!-- Static navbar -->
+<nav class="navbar navbar-default navbar-static-top">
+<div class="container">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+    <span class="sr-only">Toggle navigation</span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="http://localhost/php-bootstrap/index.php">Home</a>
+  </div>
+
+  <div id="navbar" class="collapse navbar-collapse">
+    <ul class="nav navbar-nav">
+    <li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Data Master<span class="caret"></span></a>
+
+    <ul class="dropdown-menu">
+      <li><a href="http://localhost/php-bootstrap/siswa/data-siswa.php">Data Siswa</a></li>
+      <li><a href="http://localhost/php-bootstrap/kelas/data-kls.php">Data Kelas</a></li>
+      <li><a href="http://localhost/php-bootstrap/jurusan/data-jrs.php">Data Jurusan</a></li>
+    </ul>
+
+    <li><a href="#">Data Tx<span class="sr-only">(current)</span></a>
+    </li>
+
+    <li><a href="#">Report<span class="sr-only">(current)</span></a>
+    </li>
+    </ul>
+    </li>
+    </ul>
+    </li>
+    </ul>
+  </div><!--/.nav-collapse -->
+</div>
+</nav>
